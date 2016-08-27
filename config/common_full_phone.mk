faa54eea71ee03f6293f535f906dbf78715d8015
Joe Maples <joe@frap129.org>
2016-08-27 21:28:23 +0000

    Use vendor addons
    Signed-off-by: Joe Maples <joe@frap129.org>

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -1,4 +1,4 @@
 # Inherit common stuff
 $(call inherit-product, vendor/maple/config/common_full.mk)
-
 $(call inherit-product, vendor/maple/config/telephony.mk)
+$(call inherit-product, vendor/addons/config.mk)
