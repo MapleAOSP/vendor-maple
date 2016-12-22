f100cleveland/vendor_benzo

vendor: Copy old-apns-conf.xml into builds
Signed-off-by: mydongistiny <jaysonedson@gmail.com>

config/telephony.mk
@@ -1,6 +1,6 @@
-# World APN list
-#PRODUCT_COPY_FILES += \
-#    vendor/benzo/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+# Old APNS
+PRODUCT_COPY_FILES += \
+    vendor/benzo/prebuilt/common/etc/old-apns-conf.xml:system/etc/old-apns-conf.xml
 
 # Telephony packages
 PRODUCT_PACKAGES += \

MapleAOSP/vendor-maple

Signed-off-by: Joe Maples <joe@frap129.org>
Conflicts:
	config/telephony.mk

config/telephony.mk
@@ -1,6 +1,6 @@
-# World APN list
-#PRODUCT_COPY_FILES += \
-#    vendor/maple/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+# Old APNS
+PRODUCT_COPY_FILES += \
+    vendor/maple/prebuilt/common/etc/old-apns-conf.xml:system/etc/old-apns-conf.xml
 
 # Telephony packages
 PRODUCT_PACKAGES += \